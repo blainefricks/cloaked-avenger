9202f0aadb073a6f72d471e8b2e5db2070841233
Blaine Fricks <blaine.fricks@live.com>
2015-03-19 10:24:36 -0700

    added some Views

---
+++ Subversion GUI/FileStatusView.swift
@@ -0,0 +1,18 @@
+//
+//  FileStatusView.swift
+//  Subversion GUI
+//
+//  Created by Blaine Fricks on 3/19/15.
+//  Copyright (c) 2015 Blaine Fricks. All rights reserved.
+//
+
+import Cocoa
+
+class FileStatusView: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ Subversion GUI/LogView.swift
@@ -0,0 +1,18 @@
+//
+//  LogView.swift
+//  Subversion GUI
+//
+//  Created by Blaine Fricks on 3/19/15.
+//  Copyright (c) 2015 Blaine Fricks. All rights reserved.
+//
+
+import Cocoa
+
+class LogView: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
