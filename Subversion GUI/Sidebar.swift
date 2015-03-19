2d463a0ebc327e4dfefbd3d53e19e905375f6253
Blaine Fricks <blaine.fricks@live.com>
2015-03-19 09:40:19 -0700

    added sidebar file

---
+++ Subversion GUI/Sidebar.swift
@@ -0,0 +1,18 @@
+//
+//  Sidebar.swift
+//  Subversion GUI
+//
+//  Created by Blaine Fricks on 3/19/15.
+//  Copyright (c) 2015 Blaine Fricks. All rights reserved.
+//
+
+import Cocoa
+
+class Sidebar: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
