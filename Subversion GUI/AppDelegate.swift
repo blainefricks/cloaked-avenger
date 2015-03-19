d93087072e5c8c44024681105e27a5ccd7682d51
Blaine Fricks <blaine.fricks@live.com>
2015-03-18 17:13:09 -0700

    Created Xcode project and ignored Xcode project files.

---
+++ Subversion GUI/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Subversion GUI
+//
+//  Created by Blaine Fricks on 3/18/15.
+//  Copyright (c) 2015 Blaine Fricks. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
