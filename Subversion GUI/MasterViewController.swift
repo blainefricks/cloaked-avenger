b13ce3c2228d293173f403de5f07a9d54858afd9
Blaine Fricks <blaine.fricks@live.com>
2015-03-19 09:39:51 -0700

    added MasterViewController

---
+++ Subversion GUI/MasterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.swift
+//  Subversion GUI
+//
+//  Created by Blaine Fricks on 3/19/15.
+//  Copyright (c) 2015 Blaine Fricks. All rights reserved.
+//
+
+import Cocoa
+
+class MasterViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
