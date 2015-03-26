f5fe5fb0e9a73b1b381d132341881f4d64b8b6ad
Blaine Fricks <blaine.fricks@live.com>
2015-03-26 13:03:21 -0700

    started work on split view width constraints

--- Subversion GUI/MasterViewController.swift
+++ Subversion GUI/MasterViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import Cocoa
+import AppKit
 
-class MasterViewController: NSViewController {
+class MasterViewController: NSSplitViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do view setup here.
-    }
+//  dont need this because it is already declared under NSSplitViewController
+//  var splitView: NSSplitView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do view setup here.
+
+    // Set Min, Max and Initial 
+    splitView.minPossiblePositionOfDividerAtIndex(0)
+    splitView.maxPossiblePositionOfDividerAtIndex(200)
+    splitView.setPosition(50, ofDividerAtIndex: 100)
+  }
+  
     
 }
